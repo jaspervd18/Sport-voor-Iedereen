c59f70fec845ac13a616718e536fe92cf2f5eb0e
Jasper Van Dyck <jasper.vandyck@student.hogent.be>
2022-11-15 10:16:49 +0100

    frontend

--- backend/config/development.js
+++ backend/config/development.js
@@ -11,7 +11,7 @@ module.exports = {
     client: "mysql2",
     host: "localhost",
     port: 3306,
-    name: "klauwaerts_wiki",
+    name: "schreiffout",
     username: "root",
     password: "root",
   },
---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const naam = "Jasper en Sander"
+
+
+  return (
+    <div className="App">
+      <p>{naam}</p>
+    </div>
+  );
+}
+
+export default App;
